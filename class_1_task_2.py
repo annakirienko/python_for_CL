0e8ea415ee0056d6793c9be121f031a26ac91e19
anna <anne.kirienko@gmail.com>
2019-11-02 09:01:42 +0300

    homework for classes 1 and 2

---
+++ class_1_task_2.py
@@ -0,0 +1,6 @@
+str1 = "Nobody exhists on purpose. "
+str2 = "Nobody belongs anywhere. "
+str3 = "We're all going to die. "
+str4 = "Come watch TV."
+str5 = str1 + str2 + str3 + str4
+print(str5) 
---
+++ class_1_task_4.py
@@ -0,0 +1,3 @@
+text = "WOW,NOEL SEES LEON"
+text = text.lower()[::-1]
+print(text)
